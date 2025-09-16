78c05e8e08977d22e6aefc394b74f370b125a407
Ana carolina <senac.anacarolinaribeiro@gmail.com>
2025-09-15 21:53:20 -0300

    feat(backend) #9 adiciona entidade catgoria.

--- backend/MeuCorre.Domain/Entities/Categoria.cs
+++ backend/MeuCorre.Domain/Entities/Categoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MeuCorre.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,38 @@ using System.Threading.Tasks;
 
 namespace MeuCorre.Domain.Entities
 {
-    public class Categoria
+    public class Categoria : Entidade
     {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public Guid UsuarioId { get; private set; }
+        public string Nome { get;  private set; }
+        public string? Descricao { get;private set; }
+        public string? Cor { get; private set; }
+        public string? Icone { get; private set; }
+        public bool Ativo { get; private set; }
+        public TipoTransacao TipoDaTransacao { get; private set; }
 
+
+
+        public void AtualizarInformacoes(string nome, string? descricao, string? cor, string? icone, bool ativo, TipoTransacao tipoDaTransacao)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Cor = cor;
+            Icone = icone;
+            Ativo = ativo;
+            TipoDaTransacao = tipoDaTransacao;
+            AtualizarDataMoficacao();
+        }
+
+        public void Ativar()
+        {
+            Ativo = true;
+            AtualizarDataMoficacao();
+        }
+        public void Inativar()
+        {
+            Ativo = false;
+            AtualizarDataMoficacao();
+        }
     }
 }
